0f19bcd59ae609a8447a22265d2ab238500c632d
Luis Floreani <luis.floreani@gmail.com>
2014-08-22 17:12:47 -0300

    adding scenarios in comments

--- src/FileContents.swift
+++ src/FileContents.swift
@@ -6,6 +6,24 @@
 //  Copyright (c) 2014 luisfloreani.com. All rights reserved.
 //
 
+/*
+
+*. create an index of class -> header file, so only if class is found, it will be imported (will avoid trying to import UIKit ones for instance)
+
+*. search for '\[.* new]\' or '\[.* alloc]\', if there is no import of $1, add it as #import "$1.h"
+- avoid importing NS* classes
+
+*. protocols?
+
+*. index category methods, then look for them inside the .m, if found import the header where the method declaration was found
+
+*. find unused imports!
+
+*. how to solve framework headers? <Bla/Bla.h>
+
+*/
+
+
 import Foundation
 
 class FileContents {
